82ccf8f31fc6a6af48f1d1d9661e0b507f365c68
Pranav Pokale <106576132+pranav50034@users.noreply.github.com>
2023-05-14 11:34:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name = name
+	this.age = age
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+	this.name = name
+	this.age = age
+	this.jobTitle = jobTitle
+}
+
+Person.prototype.greet = function () {
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+}
+
+Employee.prototype = Person.prototype
+
+Person.prototype.jobGreet = function () {
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)
+}
 
 // Do not change code below this line
 window.Person = Person;
